6ab95efd5cafc5d6a5d35dab2be87c6b5a668660
Herborg <Herborg@LAPTOP-5O84QV7D>
2019-02-05 11:42:43 +0000

    Updates

---
+++ precache-manifest.e0c79a9a9a9e2a2a40c474253a0a3025.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/flappy/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bf1ac435f9b0ae8282ef",
+    "url": "/flappy/static/js/runtime~main.bf1ac435.js"
+  },
+  {
+    "revision": "bd4bce975f9bb863fa5f",
+    "url": "/flappy/static/js/main.bd4bce97.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/flappy/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "bd4bce975f9bb863fa5f",
+    "url": "/flappy/static/css/main.b02a550d.chunk.css"
+  },
+  {
+    "revision": "81d7f3f084f84b8aa8ae0a496e68d75b",
+    "url": "/flappy/index.html"
+  }
+];
